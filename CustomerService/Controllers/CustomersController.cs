b0471e48a89476cc1044f3ee34e4aa7d2e9ced2e
Bryan <bryan.p.connelly@gmail.com>
2024-02-10 13:41:20 -0700

    Add project files.

---
+++ CustomerService/Controllers/CustomersController.cs
@@ -0,0 +1,44 @@
+﻿using CustomersService.DBAccessEntities;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace CustomerService.Controllers
+{
+    [ApiController]
+    [Route("CustomersService")]
+    public class CustomersController : ControllerBase
+    {
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [HttpGet("getAllCustomers")]
+        public ActionResult<string> Get()
+        {
+            return JsonSerializer.Serialize(new Customer("c sharp bob", "123 main st", 12, 2));
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"value: {id}";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CustomerService/DBAccessEntities/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomersService.DBAccessEntities
+{
+    public class Customer
+    {
+        public Customer(string firstName, string address, float cash, int tableNumber)
+        {
+            this.firstName = firstName;
+            this.address = address;
+            this.cash = cash;
+            this.tableNumber = tableNumber;
+        }
+
+        public string firstName { get; set; }
+        public string address { get; set; }
+        public float cash { get; set; }
+        public int tableNumber { get; set; }
+    }
+}
